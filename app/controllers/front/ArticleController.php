c965d8c1544d10edb8bf8234c12e9332d95c1294
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2025-02-07 16:29:27 +0000

    ajouter un class  ArticleControllleur et faire le page Article

---
+++ app/controllers/front/ArticleController.php
@@ -0,0 +1,44 @@
+
+
+<?php
+
+namespace App\Controllers\Front;
+
+use App\Core\Controller;
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        // Code for listing articles
+    }
+
+    public function show($id)
+    {
+        // Code for showing a single article
+    }
+
+    public function create()
+    {
+        // Code for creating a new article
+    }
+
+    public function store()
+    {
+        // Code for storing a new article
+    }
+
+    public function edit($id)
+    {
+        // Code for editing an article
+    }
+
+    public function update($id)
+    {
+        // Code for updating an article
+    }
+
+    public function delete($id)
+    {
+        // Code for deleting an article
+    }
+}
